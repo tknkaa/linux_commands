aa744443c7a4b714b272c3f7fb90addbb2e05c61
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-04 23:13:12 +0800

    specify directory

--- rls/src/main.rs
+++ rls/src/main.rs
@@ -3,7 +3,16 @@ use std::io;
 use std::path::Path;
 
 fn main() -> io::Result<()> {
-    let path = Path::new(".");
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    let input = buffer.trim();
+
+    let path = Path::new(input);
+    if !path.is_dir() {
+        let error_message = format!("'{}' is not a valid directory.", path.display());
+        let custom_error = io::Error::new(io::ErrorKind::NotFound, error_message);
+        return Err(custom_error);
+    }
     let entries = fs::read_dir(path)?;
     println!("Entries in '{}'", path.display());
 
